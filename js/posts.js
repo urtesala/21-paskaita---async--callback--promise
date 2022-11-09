b1dd928ef8a0dc62452e11af5fac367c769e4737
urtesala <urtesaladziute@gmail.com>
2022-11-09 15:12:02 +0300

    generating cards

--- js/posts.js
+++ js/posts.js
@@ -19,17 +19,34 @@ function getPosts() {
     });
   }, 2000);
 }
+//=============================
 
 //create post funckija kuri ideda nauja posta i post masyva
 
-function createPost(newPostObj) {
-  setTimeout(() => {
-    posts.push(newPostObj);
-    getPosts(); // 1 uzd
-  }, 2000);
-}
+// function createPost(newPostObj) {
+//   setTimeout(() => {
+//     posts.push(newPostObj);
+//     getPosts(); // 1 uzd
+//   }, 2000);
+// }
 
-createPost({ title: "Post three", body: "This is post 3 body" }, getPosts); //2 uzd
+// createPost({ title: "Post three", body: "This is post 3 body" }, getPosts); //2 uzd
+
+//====================
+
+function createPost(newPostObj) {
+  posts.push(newPostObj);
+  getPosts();
+  return new Promise((resolve, reject) => {
+    let noError = true;
+    if (noError) {
+      resolve("OK");
+    } else {
+      reject("BAD BAD THING HAPPENED");
+    }
+  });
+}
+createPost({ title: "Post three", body: "This is post 3 body" });
 
 // 1 iskviesti getPosts tik po to kai sukurem posta su createPost su pavadinimu
 // 2 iskviesti getPosts tik po to kai sukurem posta su createPost su callback fn
