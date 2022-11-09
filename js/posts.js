8279095ba1a82ac879b4ebd8cf63fa50a241c0b5
urtesala <urtesaladziute@gmail.com>
2022-11-09 09:47:51 +0300

    creating post using callback

--- js/posts.js
+++ js/posts.js
@@ -3,3 +3,33 @@ const posts = [
 
   { title: "Post Two", body: "This is post Two body" },
 ];
+
+//nuskaitoma
+const postsListEl = document.getElementById("posts");
+
+// gauti postus imituojant kad trunka 2sec juos gauti
+
+function getPosts() {
+  setTimeout(() => {
+    // sugeneruoti ir patalptinti posts masyvo elementa
+    posts.forEach((pObj) => {
+      const liEl = document.createElement("li");
+      liEl.textContent = `${pObj.title} - ${pObj.body}`;
+      postsListEl.append(liEl);
+    });
+  }, 2000);
+}
+
+//create post funckija kuri ideda nauja posta i post masyva
+
+function createPost(newPostObj) {
+  setTimeout(() => {
+    posts.push(newPostObj);
+    getPosts(); // 1 uzd
+  }, 2000);
+}
+
+createPost({ title: "Post three", body: "This is post 3 body" }, getPosts); //2 uzd
+
+// 1 iskviesti getPosts tik po to kai sukurem posta su createPost su pavadinimu
+// 2 iskviesti getPosts tik po to kai sukurem posta su createPost su callback fn
