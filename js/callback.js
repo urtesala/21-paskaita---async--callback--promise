cfe104e231062fb1e7ac298d5d2b49c70b918613
urtesala <urtesaladziute@gmail.com>
2022-11-09 09:23:22 +0300

    callback

--- js/callback.js
+++ js/callback.js
@@ -1,21 +1,36 @@
 "use strict";
 console.log("callback.js");
 
-function getPost1() {
+function getPost1(callBackFunc) {
   setTimeout(() => {
-    console.log("Post 1");
+    console.log("post1");
+    // cia as noriu kviesti post2
+    callBackFunc();
   }, 3000);
 }
 function getPost2() {
   setTimeout(() => {
-    console.log("Post 2");
+    console.log("post2");
   }, 2000);
 }
 function getPost3() {
   setTimeout(() => {
-    console.log("Post 3");
+    console.log("post3");
   }, 1800);
 }
-getPost1();
-getPost2();
-getPost3();
+
+getPost1(getPost2);
+// getPost2();
+// getPost3();
+
+// kada call back gali buti naudojamas
+function makeNum(cb) {
+  const rez = cb(5);
+  return rez;
+}
+
+function double(num) {
+  return num * 2;
+}
+const n1 = makeNum(double);
+// double(n1);
---
+++ js/posts.js
@@ -0,0 +1,5 @@
+const posts = [
+  { title: "Post One", body: "This is post One body" },
+
+  { title: "Post Two", body: "This is post Two body" },
+];
